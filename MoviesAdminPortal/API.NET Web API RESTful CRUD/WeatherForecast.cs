3d1e97c75fc067c03a47623dbb79fff2aaa24c7f
LaviDev <lffudulu@gmail.com>
2025-02-03 14:10:09 +0100

    Inicio API.NET 8.0

---
+++ MoviesAdminPortal/API.NET Web API RESTful CRUD/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace API.NET_Web_API_RESTful_CRUD
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
